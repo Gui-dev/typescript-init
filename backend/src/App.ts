cacc086cd5bb49a23f963991a273cc2e54c9b249
Gui-dev <adrianon10@gmail.com>
2020-07-14 18:44:08 -0300

    Configurações iniciais

---
+++ backend/src/App.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+
+import routes from './routes'
+
+class App {
+
+  public app: express.Application
+
+  constructor() {
+
+    this.app = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.app.use( express.json() )
+    this.app.use( cors() )
+  }
+
+  routes() {
+    this.app.use( routes )
+  }
+
+}
+
+export default new App().app
